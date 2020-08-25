299763df81937e29f8263f043e22a707b812f62d
Harry Pidcock <harry.pidcock@canonical.com>
2020-08-25 15:10:35 +1000

    Add ssh

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/hpidcock/juju-fake-init
 
 go 1.15
 
-require github.com/gonuts/go-shellquote v0.0.0-20180428030007-95032a82bc51
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/creack/pty v1.1.11
+	github.com/gliderlabs/ssh v0.3.0
+	github.com/gonuts/go-shellquote v0.0.0-20180428030007-95032a82bc51
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net"
@@ -9,15 +11,126 @@ import (
 	"os/exec"
 	"os/signal"
 	"sync"
+	"syscall"
+	"unsafe"
 
+	"github.com/creack/pty"
+	"github.com/gliderlabs/ssh"
 	"github.com/gonuts/go-shellquote"
 )
 
 func main() {
-	l, err := net.Listen("tcp", ":4321")
+	code, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	os.Exit(code)
+}
+
+func run() (int, error) {
+	l, err := net.Listen("tcp", "127.0.0.1:4321")
+	if err != nil {
+		return 0, err
+	}
+	sshl, err := net.Listen("tcp", "127.0.0.1:4322")
+	if err != nil {
+		return 0, err
+	}
+	defer sshl.Close()
+
+	ssh.Handle(func(s ssh.Session) {
+		req, windowChannel, isPty := s.Pty()
+		if !isPty {
+			cmd := s.Command()
+			if len(cmd) == 0 {
+				io.WriteString(s, "No PTY requested.\n")
+				s.Exit(1)
+			}
+			sc := make(chan ssh.Signal, 1)
+			defer close(sc)
+			s.Signals(sc)
+			c := exec.Command(cmd[0], cmd[1:]...)
+			c.Stdin = s
+			c.Stdout = s
+			c.Stderr = s.Stderr()
+			err = c.Start()
+			if exit, ok := err.(*exec.ExitError); ok {
+				s.Exit(exit.ExitCode())
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			go func() {
+				for {
+					sig, ok := <-sc
+					if !ok {
+						return
+					}
+					switch sig {
+					case ssh.SIGABRT:
+						c.Process.Signal(syscall.SIGABRT)
+					case ssh.SIGALRM:
+						c.Process.Signal(syscall.SIGALRM)
+					case ssh.SIGFPE:
+						c.Process.Signal(syscall.SIGFPE)
+					case ssh.SIGHUP:
+						c.Process.Signal(syscall.SIGHUP)
+					case ssh.SIGILL:
+						c.Process.Signal(syscall.SIGILL)
+					case ssh.SIGINT:
+						c.Process.Signal(syscall.SIGINT)
+					case ssh.SIGKILL:
+						c.Process.Signal(syscall.SIGKILL)
+					case ssh.SIGPIPE:
+						c.Process.Signal(syscall.SIGPIPE)
+					case ssh.SIGQUIT:
+						c.Process.Signal(syscall.SIGQUIT)
+					case ssh.SIGSEGV:
+						c.Process.Signal(syscall.SIGSEGV)
+					case ssh.SIGTERM:
+						c.Process.Signal(syscall.SIGTERM)
+					case ssh.SIGUSR1:
+						c.Process.Signal(syscall.SIGUSR1)
+					case ssh.SIGUSR2:
+						c.Process.Signal(syscall.SIGUSR2)
+					}
+				}
+			}()
+			err = c.Wait()
+			if exit, ok := err.(*exec.ExitError); ok {
+				s.Exit(exit.ExitCode())
+			} else if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+		cmd := exec.Command("sh")
+		cmd.Env = append(cmd.Env, fmt.Sprintf("TERM=%s", req.Term))
+		f, err := pty.Start(cmd)
+		if err != nil {
+			io.WriteString(s, err.Error())
+			s.Exit(1)
+		}
+		go func() {
+			for win := range windowChannel {
+				syscall.Syscall(syscall.SYS_IOCTL, f.Fd(), uintptr(syscall.TIOCSWINSZ),
+					uintptr(unsafe.Pointer(&struct{ h, w, x, y uint16 }{uint16(win.Height), uint16(win.Width), 0, 0})))
+			}
+		}()
+		go func() {
+			io.Copy(f, s) // stdin
+		}()
+		io.Copy(s, f) // stdout
+		cmd.Wait()
+	})
+
+	go func() {
+		err = ssh.Serve(sshl, nil, ssh.PublicKeyAuth(func(ctx ssh.Context, key ssh.PublicKey) bool {
+			return true
+		}))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	mut := sync.Mutex{}
 	cmd := ""
@@ -53,12 +166,12 @@ func main() {
 	err = s.Serve(l)
 	mut.Lock()
 	if err != nil && cmd == "" {
-		log.Fatal(err)
+		return 0, err
 	}
 
 	args, err := shellquote.Split(cmd)
 	if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 
 	c := exec.Command(args[0], args[1:]...)
@@ -67,9 +180,9 @@ func main() {
 	c.Stderr = os.Stderr
 	err = c.Start()
 	if exit, ok := err.(*exec.ExitError); ok {
-		os.Exit(exit.ExitCode())
+		return exit.ExitCode(), nil
 	} else if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
 
 	sc := make(chan os.Signal, 1)
@@ -86,9 +199,9 @@ func main() {
 
 	err = c.Wait()
 	if exit, ok := err.(*exec.ExitError); ok {
-		os.Exit(exit.ExitCode())
+		return exit.ExitCode(), nil
 	} else if err != nil {
-		log.Fatal(err)
+		return 0, err
 	}
-	os.Exit(0)
+	return 0, nil
 }
